6fc5fc55a9185ceb35c39c2a9fa33ece0e7bdeb4
walruzzz <ivzlatomr@gmail.com>
2026-09-12 10:15:54 +0300

    switch method added

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+void switch(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 int main()
 {
 	int x = 5;
+	int y = 10;
 
 	printf("Hello, World!\n");
 	return 0;
